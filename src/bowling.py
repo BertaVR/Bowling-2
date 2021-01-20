46a42907297bec21aef2ba843bd7da98cb5c4629
Berta Vergés <72273706+BertaVR@users.noreply.github.com>
2021-01-20 22:22:58 +0100

    refactor: :art: he cambiado algún nombrecillo

--- src/bowling.py
+++ src/bowling.py
@@ -1,19 +1,22 @@
 class Puntuacion_total_bolos:
+    ##### defino las constantes en global#######
+    ##### cuáles son los símbolos que representan cada tipo de tirada especial (entendiendo por especial la que no es open)
     NULO = '-'
     SPARE = '/'
     STRIKE = 'X'
+    ####### máximo de bolos y máximo de frames 
     MAX_BOLOS = 10
-    MAX_TURNOS = 10
+    MAX_FRAMES = 10
     
     def __init__(self, tabla_tiradas):
         self.tabla_tiradas = list(tabla_tiradas)
-        self.puntuacion = 0
-        self.total_tiradas= 0
-        self.tiradas_por_turno = 0
-        self.turnos = 1
-        self.ultimo_numero = 0
+        self.puntuacion = 0 ######## La puntuación esta se irá actualizando con cada función####
+        self.total_tiradas= 0 #### esto es para saber la posición, necesario para calcular las tiradas extra en el spare y el strike
+        self.tiradas_por_frame = 0 #necesario para actualizar los frames, en las puntuaciones open y nulo será necesario para poder actualizar cuando llega el siguiente frametiradas_por_frame (cuando llegue a 2 sumará 1 frametiradas_por_frame)
+        self.frames = 1 #este 1 lo pongo para que el frame especial sea el número 10 
+        self.ultimo_numero = 0 ##### esto es para calcular la puntuacion del spare restando el último número que ha salido. Se va actualizando en cada open y cada nulo (en spare y strike no porque no puede haber un spare después de un strike o un spare)
 
-    def equivalencias_puntuacion_simbolo(self, lista_tiradas):
+    def equivalencias_puntuacion_simbolo(self, lista_tiradas): ###esto no está en el diagrama del modelo pero ha sido necesario añadirlo para implementar la lógica.
         puntuacion = 0
         for tirada in lista_tiradas:
             if tirada == Puntuacion_total_bolos.NULO:
@@ -21,7 +24,7 @@ class Puntuacion_total_bolos:
                 self.ultimo_numero = 0
             if tirada.isdigit():
                 puntuacion += int(tirada)
-                self.ultimo_numero = tirada
+                self.ultimo_numero = tirada  ### hay que actualizar el último número
             if tirada == Puntuacion_total_bolos.SPARE:
                 puntuacion += Puntuacion_total_bolos.MAX_BOLOS - int(self.ultimo_numero) #int porque detrás de un spare solo puede ir un número (el numero de bolos que has tirado)
             if tirada == Puntuacion_total_bolos.STRIKE:
@@ -31,8 +34,8 @@ class Puntuacion_total_bolos:
 
 
     def puntuacion_partida(self):
-        for tirada in self.tabla_tiradas:
-            if self.turnos < Puntuacion_total_bolos.MAX_TURNOS:
+        for tirada in self.tabla_tiradas: ###esta función recorre la tabla de tiradas y distribuye: le pasa cada puntuación de tirada a la función correspondiente (open, strike...)
+            if self.frames < Puntuacion_total_bolos.MAX_FRAMES:
                 if tirada.isdigit():
                     Puntuacion_total_bolos.puntuacion_open(self, tirada)
                 if tirada == Puntuacion_total_bolos.NULO:
@@ -41,44 +44,45 @@ class Puntuacion_total_bolos:
                     self.puntuacion += Puntuacion_total_bolos.puntuacion_spare(self)
                 if tirada == Puntuacion_total_bolos.STRIKE:
                     self.puntuacion += Puntuacion_total_bolos.puntuacion_strike(self)
-            elif self.turnos == Puntuacion_total_bolos.MAX_TURNOS:
+                ######FRAME ESPECIAL########## -Lo pasa a una función que simplemente sumará todo de ahí en adelante (tomando las equivalencias ente la puntuación y el símbolo). Cuando el frametiradas_por_frame sea el 10 (lo he "hackeado un poquito empezando los frames desde 1 y no desde 0")
+            elif self.frames == Puntuacion_total_bolos.MAX_FRAMES:
                     self.puntuacion += Puntuacion_total_bolos.puntuacion_tenth(self)
-                    return self.puntuacion
-            self.total_tiradas += 1
+                    return self.puntuacion ###esto va aquí pq quiero que se ejecute después de haber contado la última tirada. como sólo necesitamos la puntuación final (y no puntuaciones intermedias) no molesta
+            self.total_tiradas += 1 #actualizamos la posición de las tiradas en cada iteración del for
 
 
     
     def puntuacion_open(self, tirada):
-        self.tiradas_por_turno += 1
-        self.puntuacion += int(tirada)
-        if self.tiradas_por_turno == 2:
-            self.turnos += 1
-            self.tiradas_por_turno = 0
+        self.tiradas_por_frame += 1 ##hemos hecho 1 tirada, la sumamos al frametiradas_por_frame (esto no lo hacemos en puntuacion_partida() pq por ejemplo los strikes suman diferente)
+        self.puntuacion += int(tirada) ##sumar tirada
+        if self.tiradas_por_frame == 2: ##cada vez que sea la segunda tirada del frametiradas_por_frame reiniciamos el contador de tiradas por frametiradas_por_frame y actualizamos frametiradas_por_frame sumando 1.
+            self.frames += 1
+            self.tiradas_por_frame = 0
         self.ultimo_numero = tirada
 
     def puntuacion_nulo(self, tirada):
-        self.puntuacion += 0
-        self.tiradas_por_turno += 1
-        if self.tiradas_por_turno == 2:
-            self.turnos += 1
-            self.tiradas_por_turno = 0
-        self.ultimo_numero = 0            
+        CERO = 0
+        self.puntuacion += CERO ##si es nulo la puntuación es 0
+        self.tiradas_por_frame += 1
+        if self.tiradas_por_frame == 2: ##cada vez que sea la segunda tirada del frametiradas_por_frame reiniciamos el contador de tiradas por frametiradas_por_frame y actualizamos frametiradas_por_frame sumando 1.
+            self.frames += 1
+            self.tiradas_por_frame = 0 ## Aquí no va como constante el 0 porque es diferente, no es 0 por ser nulo, es 0 porque hay que reiniciar el contador cuando llega a 2 
+        self.ultimo_numero = CERO            
 
     
     def puntuacion_spare(self):
-        lista_tiradas = self.tabla_tiradas[self.total_tiradas : self.total_tiradas + 2]
-        self.turnos += 1
-        self.tiradas_por_turno = 0  
+        lista_tiradas = self.tabla_tiradas[self.total_tiradas : self.total_tiradas + 2] ##le paso esta tirada y la siguiente a la función que interpreta símbolos
+        self.frames += 1  ##siempre que hay spare es en la segunda tirada, podemmos simplemete actualizar el frametiradas_por_frame y el contador de tiradas por frametiradas_por_frame
+        self.tiradas_por_frame = 0  
         return Puntuacion_total_bolos.equivalencias_puntuacion_simbolo(self, lista_tiradas)
 
 
     def puntuacion_strike(self):
-        lista_tiradas = self.tabla_tiradas[self.total_tiradas : self.total_tiradas + 3]
-        self.turnos += 1
+        lista_tiradas = self.tabla_tiradas[self.total_tiradas : self.total_tiradas + 3] ###pasas esta tirada y las 2 siguientes 
+        self.frames += 1 ##siempre que hay strike se salta de frametiradas_por_frame
         return Puntuacion_total_bolos.equivalencias_puntuacion_simbolo(self, lista_tiradas)
 
-    def puntuacion_tenth(self):
-        lista_tiradas = self.tabla_tiradas[self.total_tiradas :]
+    def puntuacion_tenth(self): ###el nombre tenth puede ser cambiable por última tirada, teniendo en cuenta que se modificaran el número de tiradas que tiene el juego, pero la logica es la misma.
+        lista_tiradas = self.tabla_tiradas[self.total_tiradas :] ##pasa de la tirada en adelante y se dedicará solo a sumar simbolitos, sin más lógica.
         return Puntuacion_total_bolos.equivalencias_puntuacion_simbolo(self, lista_tiradas)
-if __name__ == '__main__':
-    assert Puntuacion_total_bolos("8/549-XX5/53639/9/X").puntuacion_partida() == 149
+
